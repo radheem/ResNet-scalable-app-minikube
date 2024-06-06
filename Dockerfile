196e7eca6b8d325429aa4ebfae80653cc31b5e01
radheem1 <radheem.razi@hilalinvest.com>
2024-06-06 10:05:02 +0200

    step 1 complete

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-# syntax=docker/dockerfile:1
-
 FROM python:3.10-alpine
 
-WORKDIR /app
+ADD . /code
 
-COPY requirements.txt requirements.txt
+WORKDIR /code
 
-RUN pip3 install -r requirements.txt
+RUN pip install -r requirements.txt
 
-COPY . .
-
-CMD ["python", "app.py"]
-# CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+CMD python app.py
--- app.py
+++ app.py
@@ -1,40 +1,17 @@
-from dotenv import load_dotenv
-import os
-
-# load environment variables from .env file
-load_dotenv()
 from flask import Flask
 from redis import Redis
+from os import environ
+
+redis_host = environ.get('REDIS_HOST', 'localhost')
+redis_port = environ.get('REDIS_PORT', 6379)
 
 app = Flask(__name__)
-redis_host = os.getenv("HOST")
-redis_port = os.getenv("PORT")
-def connect_redis():
-    try:
-        redis = Redis(host=redis_port, port=redis_port, db=0, socket_timeout=5)
-        redis.ping()
-        return redis
-    except Exception as e:
-        print("Error: ", e)
-        return False
-    
-redis = connect_redis()
-
-@app.route('/counter')
-def redisFunc():
-    global redis
-    if not redis:
-        redis = connect_redis()
-        if not redis:
-            return "Error connecting to Redis"
-    redis.incr('hits')
-    counter = str(redis.get('hits'),'utf-8')
-    return f"Welcome to Redis test!, view count: " + counter
+redis = Redis(host=redis_host, port=redis_port)
 
 @app.route('/')
 def hello():
-    return f"Welcome to redis: port>30379 . Hit /counter to test view count" 
-
+    redis.incr('hits')
+    return 'Hello World! I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
