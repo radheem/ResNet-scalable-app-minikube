61ae85b6cc2213323ebed82c5386f55f9fff5a3a
radheem1 <radheem.razi@hilalinvest.com>
2024-06-08 00:29:18 +0200

    adding prometheus monitoring

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM python:2.7
-ADD . /code
-WORKDIR /code
-RUN pip install -r requirements.txt
-CMD python app.py
+FROM python:3.10-alpine
 
-# docker build -t web .
+ADD . /code
+
+WORKDIR /code
+
+RUN pip install -r requirements.txt
+
+CMD python app.py
--- app.py
+++ app.py
@@ -1,17 +1,50 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest, REGISTRY
+from prometheus_client.exposition import start_http_server
 from redis import Redis
 from os import environ
+import json
 
+# Get environment variables with default values
 redis_host = environ.get('REDIS_HOST', 'localhost')
-redis_port = environ.get('REDIS_PORT', 6379)
+redis_port = int(environ.get('REDIS_PORT', 6379))
+app_port = int(environ.get('PORT', 5000))
+metrics_port = int(environ.get('METRICS_PORT', 8000))
 
 app = Flask(__name__)
 redis = Redis(host=redis_host, port=redis_port)
 
-@app.route('/')
-def hello():
-    redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+# Define a Prometheus counter for tracking hits
+route_hit_counter = Counter('route_hits', 'Count of hits to routes', ['route'])
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+@app.route('/')
+def index():
+    route_hit_counter.labels(route='/').inc()
+    redis.incr('/')
+    return "Hello, World!"
+
+@app.route('/counter')
+def counter():
+    redis.incr('/counter')
+    route_hit_counter.labels(route='/counter').inc()
+    count = redis.get('/counter').decode('utf-8')
+    return f"visited {count} times"
+
+@app.route('/redis')
+def redis_data():
+    data = {
+        "/": redis.get('/').decode('utf-8') if redis.get('/') else 0,
+        "/counter": redis.get('/counter').decode('utf-8') if redis.get('/counter') else 0,
+        "/metrics": redis.get('/metrics').decode('utf-8') if redis.get('/metrics') else 0
+    }
+    return json.dumps(data)
+
+@app.route('/metrics')
+def metrics():
+    redis.incr('/metrics')
+    return generate_latest(REGISTRY), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+if __name__ == '__main__':
+    # Start the Prometheus metrics server on the specified port
+    start_http_server(metrics_port)
+    app.run(host="0.0.0.0", port=app_port)
