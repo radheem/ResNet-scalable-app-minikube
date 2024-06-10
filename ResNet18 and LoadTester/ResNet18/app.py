a4e9b61caf9d65fb1a164d6c67819d1ac9790b52
AbdulMoiz001 <s.abdulmoiz001@gmail.com>
2024-06-10 22:55:00 +0200

    added docker file and its working properly for ResNet18. also renamed the folders and files to better match the information

---
+++ ResNet18 and LoadTester/ResNet18/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import os
+import io
+from PIL import Image
+from  ResNet18 import ImageClassifier  # ImagePredictor class is in a separate file called ResNet18.py
+
+app = Flask(__name__)
+
+# Assuming the ImagePredictor is correctly implemented
+classifier = ImageClassifier()
+
+@app.route('/image_prediction', methods=['POST'])
+def image_prediction():
+    # Check if there is data in the request
+    if not request.data:
+        return jsonify({'error': 'No data in the request'}), 400
+
+    # Try to open the image from the raw binary data
+    try:
+        image = Image.open(io.BytesIO(request.data))
+    except Exception as e:
+        return jsonify({'error': 'Invalid image data'}), 400
+
+    # Perform the prediction
+    try:
+        results = classifier.predict(image, topk=2)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    return jsonify(results), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
